d8ee1969d991054f744d4d9a3802a58f88dfab57
Eddy Briand <333ebr@gmail.com>
2022-08-09 14:03:04 +0200

    feat: theme doesn't reset between pages

--- docs/dark.js
+++ docs/dark.js
@@ -1,22 +1,29 @@
-const btn = document.querySelector(".btn-dark");
-
-const currentTheme = localStorage.getItem("theme");
+var currentTheme = localStorage.getItem("theme");
+var checkboxes = document.getElementsByClassName("btn-dark__checkbox");
+var element = document.documentElement;
 
 if (currentTheme == "dark") {
-  if (!document.body.classList.contains("dark")) {
+  if (!element.classList.contains("dark")) {
     element.classList.toggle("dark");
   }
+  Array.from(checkboxes).forEach((checkbox) => {
+    checkbox.checked = true;
+    console.log("dark", checkbox);
+  });
 } else if (currentTheme == "light") {
-  if (document.body.classList.contains("dark")) {
+  if (element.classList.contains("dark")) {
     element.classList.toggle("dark");
   }
+  Array.from(checkboxes).forEach((checkbox) => {
+    checkbox.checked = false;
+    console.log("light", checkbox);
+  });
 }
 
 function toggleDark() {
-  var element = document.documentElement;
   element.classList.toggle("dark");
 
-  if (document.body.classList.contains("dark")) {
+  if (element.classList.contains("dark")) {
     localStorage.setItem("theme", "dark");
   } else {
     localStorage.setItem("theme", "light");
